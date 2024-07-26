ad97c2a4487ddcfd3568795b9671c122c5d3736d
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-07-26 05:27:24 -0400

    Opdating constants

---
+++ app/src/main/java/com/example/project_g04/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.project_g04.utils
+
+object Constants {
+    const val SHARED_PREF_NAME = "KotlinSharedPreference"
+    const val USER_KEY = "user_key"
+    const val COURSE_KEY = "course_key"
+    const val EXTRA_USER_NAME = "EXTRA_USER_NAME"
+    const val EXTRA_LESSON = "EXTRA_LESSON"
+}

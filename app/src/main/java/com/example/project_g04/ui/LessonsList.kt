48e195ee9bc6e2b5ec072828617f261f574654b5
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-07-26 04:58:40 -0400

    Upating LessonList to get data from SharedPreferences

--- app/src/main/java/com/example/project_g04/ui/LessonsList.kt
+++ app/src/main/java/com/example/project_g04/ui/LessonsList.kt
@@ -6,25 +6,50 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project_g04.adapters.LessonAdapter
-import com.example.project_g04.data.Mock
 import com.example.project_g04.databinding.ActivityLessonsListBinding
 import com.example.project_g04.interfaces.ClickDetectorInterface
 import com.example.project_g04.models.Course
-import com.example.project_g04.models.Lesson
+import com.example.project_g04.utils.Constants
+import com.example.project_g04.utils.SharedPreferencesManager
+import com.example.project_g04.utils.SnackbarHelper
+import com.google.gson.Gson
 
 class LessonsList : AppCompatActivity(), ClickDetectorInterface {
     private lateinit var binding: ActivityLessonsListBinding
     private lateinit var adapter: LessonAdapter
     private lateinit var course: Course
+    private lateinit var sharedPreferencesManager: SharedPreferencesManager
+    private lateinit var snackbarHelper: SnackbarHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLessonsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        this.snackbarHelper = SnackbarHelper(this.binding.root)
+        this.sharedPreferencesManager = SharedPreferencesManager(this, Constants.SHARED_PREF_NAME)
+    }
 
-        // getting mock data
-        this.course = Mock.COURSE
+    override fun onResume() {
+        super.onResume()
+         val loadedCourse: Course? = getCourseProgressFromSharedPreferences()
+        if (loadedCourse !== null) {
+            this.course = loadedCourse
+            loadAdapter()
+        } else {
+            finish()
+        }
+    }
 
+    private fun getCourseProgressFromSharedPreferences(): Course? {
+        val courseJSON = this.sharedPreferencesManager.getString(Constants.COURSE_KEY, "")
+        if (courseJSON.isNullOrEmpty()) {
+            return null
+        }
+        val course = Gson().fromJson(courseJSON, Course::class.java)
+        return course
+    }
+
+    private fun loadAdapter() {
         // create an instance of the custom adapter
         this.adapter = LessonAdapter(this, this.course.lessons.toMutableList(), this)
         this.binding.rvContainer.adapter = this.adapter
